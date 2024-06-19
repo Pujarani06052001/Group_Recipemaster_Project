9b118e522e385f8a82a9dd7c5bd14143e6fa3544
Akhila1122 <akhila22@navgurkul.org>
2024-06-19 12:31:13 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import Our from './components/Our';
+import Glossary from './components/Glossary';
+import TipsAndTricks from './components/TipsAndTricks';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [activeButton, setActiveButton] = useState('TipsAndTricks');
+
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => handleButtonClick('TipsAndTricks')}>Tips&Tricks</button>
+        <button onClick={() => handleButtonClick('Glossary')}>Glossary</button>
+        <button onClick={() => handleButtonClick('Our')}>Our Programs</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        {activeButton === 'TipsAndTricks' && <TipsAndTricks />}
+        {activeButton === 'Glossary' && <Glossary />}
+        {activeButton === 'Our' && <Our />}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
