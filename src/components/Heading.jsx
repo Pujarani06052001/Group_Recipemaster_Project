f2c93266bcd587a4ba29fc44af833b5eae04b567
Ashna123 <ashna22@navgurukul.org>
2024-06-27 22:03:08 +0530

    added code

--- src/components/Heading.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Heading(){
-    return(<>
-
-<h3 style={{ marginLeft: '650px' , marginTop:'20px', color:'black'}}>Quick Recipe</h3>
-
-    </>)
-}
-export default Heading;
